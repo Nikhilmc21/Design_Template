714351f38b1c8b8b1d5033adaf542496ac537662
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2023-01-23 21:24:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,5 +19,8 @@ function agreelocation(){
 let apiKey = 'd9e53816d07345139c58d0ea733e3870';
 $.getJSON('https://api.bigdatacloud.net/data/ip-geolocation?key=' + apiKey, function(data) {
   console.log(JSON.stringify(data, null, 2));
+  localStorage.setItem("IP", data.ip);
 });
+var ip = localStorage.getItem("IP");
+document.getElementById("").innerHTML = ip;
 };
